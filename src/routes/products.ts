a01d7a125cebe9b17226fcb4f040f8ee9c245318
Kenneth <kennethlokoy@gmail.com>
2022-08-20 14:43:41 +0200

    Update products.ts

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -7,13 +7,23 @@ const productsRouter = Router()
 productsRouter.get('/', async function(req, res) {
   try {
     const client = await clientPromise
-    const products = await client.db(process.env.MONGODB_DB_NAME).collection('Products').find().toArray()
+    const db = client.db(process.env.MONGODB_DB_NAME) 
+    const products = await db.collection('Products').find().toArray()
+    const categories = await db.collection('Categories')
 
-    res.status(200).send(products.map(product => ({
-      ...product,
-      image: req.secure
-        ? `https://${req.get('host')}/img/${product.image}.jpg`
-        : `http://${req.get('host')}/img/${product.image}.jpg`
+    res.status(200).send(await Promise.all(products.map(async product => {
+      const category = await categories.findOne({_id: product.category})
+      
+      return {
+        ...product,
+        category: {
+          slug: category?.slug || '',
+          name: category?.name || ''
+        },
+        image: req.secure
+          ? `https://${req.get('host')}/img/${product.image}.jpg`
+          : `http://${req.get('host')}/img/${product.image}.jpg`
+      }
     })))
   
   } catch (err) {
