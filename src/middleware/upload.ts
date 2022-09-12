d4d2d4aeda95e861ead0e49bab4a6eb1fe75d6a8
klokoy <kennethlokoy@gmail.com>
2022-09-12 12:57:34 +0200

    Update upload.ts

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,10 +1,15 @@
 import multer from 'multer'
 import { GridFsStorage } from 'multer-gridfs-storage'
-import mongoose from 'mongoose'
+import { MongoClient } from 'mongodb'
+
+const dbPromise =
+  MongoClient
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(client => client.db(process.env.MONGODB_DB_NAME))
 
 const storage = new GridFsStorage({
   url: process.env.MONGODB_CONNECTION_STRING,
-  db: mongoose.connection.db,
+  db: dbPromise,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true
