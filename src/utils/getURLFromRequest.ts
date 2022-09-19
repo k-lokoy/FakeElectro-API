4d612b6bfced0954014a4278ee0e7aae42af9e31
klokoy <kennethlokoy@gmail.com>
2022-09-19 12:05:19 +0200

    Update getURLFromRequest.ts

--- src/utils/getURLFromRequest.ts
+++ src/utils/getURLFromRequest.ts
@@ -1,5 +1,3 @@
 export default function getURLFromRequest(req) {
-  const port = !process.env.PORT || ['8080', '80'].includes(process.env.PORT) ? '' : `:${process.env.PORT}`
-  
-  return `${req.protocol}://${req.hostname}${port}`
+  return `${req.protocol}://${req.hostname}`
 }
