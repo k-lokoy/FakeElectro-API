1499e4e0072a98501527f838b3186a06cc372100
klokoy <kennethlokoy@gmail.com>
2022-09-24 12:14:03 +0200

    Removed Node version

--- src/utils/getURLFromRequest.ts
+++
@@ -1,5 +0,0 @@
-export default function getURLFromRequest(req) {
-  return 'development' === process.env.NODE_ENV
-    ? `${req.protocol}://${req.hostname}:${process.env.PORT}`
-    : `${req.protocol}://${req.hostname}`
-}
